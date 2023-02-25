44ceb64b248356d7d2ce810a6f1b633b6f273729
Trevor_Lawson <trevor3lawson@gmail.com>
2023-02-25 14:15:09 -0700

    Add project files.

---
+++ Mission09_trev3law/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_trev3law.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_trev3law.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission09_trev3law/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_trev3law.Models
+{
+    public class Test
+    {
+    }
+}
