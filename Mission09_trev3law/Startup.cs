90d716d8a9bd39add582f5ab09bad7002ed1a495
Trevor_Lawson <trevor3lawson@gmail.com>
2023-03-04 13:43:46 -0700

    URL fixes and BookSummary partial view

--- Mission09_trev3law/Startup.cs
+++ Mission09_trev3law/Startup.cs
@@ -58,6 +58,11 @@ namespace Mission09_trev3law
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "Paging",
+                    pattern: "Page{pageNum}",
+                    defaults: new { Controller = "Home", action = "Index" });
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
